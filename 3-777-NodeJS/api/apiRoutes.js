2f8bdf0fba6ae74ef80f7f2da5986ff4f2fe8168
Степанов Юрий <sup710@yandex.ru>
2020-04-14 17:49:07 +0300

    SHRIHOMEWORKIII-777: Убраны вспомогательные методы из файла с маршрутами (NodeJS)

--- 3-777-NodeJS/api/apiRoutes.js
+++ 3-777-NodeJS/api/apiRoutes.js
@@ -1,72 +1,11 @@
 const { Router } = require('express');
-const axios = require('axios').default;
-const { Agent } = require('https');
 const swaggerUi = require('swagger-ui-express');
 const swaggerConfig = require('./swagger/swagger.json');
-const { apiToken } = require('../config');
-const { getCommitByHash, runGitClone } = require('../git/gitHelper');
+const { requireAuthenticationToDB } = require('../db/dbHelper');
+const { getSettingsRoute, saveSettingsRoute } = require('./settings/settingsRoutesHelper');
+const { getBuildListRoute, saveNewBuildRoute, getBuildDetailsRoute, getBuildLogRoute } = require('./builds/buildRoutesHelper');
 
 const router = Router();
-
-const axiosDBInstance = axios.create({
-    baseURL: 'https://hw.shri.yandex/api',
-    timeout: 5000,
-    headers: {
-        Authorization: 'Bearer',
-    },
-    httpsAgent: new Agent({
-        rejectUnauthorized: false,
-    }),
-});
-
-const requireAuthenticationToDB = (req, res, next) => {
-    let authorization = req.get('authorization');
-
-    if (!authorization && apiToken) {
-        // Установка специального токена из файла конфигурации
-        authorization = `Bearer ${apiToken}`;
-    }
-
-    if (authorization) {
-        const [scheme, key] = authorization.toString().split(' ');
-        if (scheme === 'Bearer' && key) {
-            // Установка токена в заголовке Authorization
-            axiosDBInstance.defaults.headers.Authorization = authorization;
-
-            return next();
-        }
-    }
-
-    const code = 401;
-    const message = 'Возможны 2 варианта авторизации в БД:'
-        + ' 1. В headers передать authorization: "Bearer {apiToken}".'
-        + ' Для этого можно использовать кнопку Authorize в Swagger'
-        + ' 2. В .env файле указать apiToken, как в файле примере(.env.example).'
-        + ' Не забыть потом перезапустить сервер';
-    return res.status(code).json({ code, message });
-};
-
-const getCodeAndMessage = (error) => {
-    // eslint-disable-next-line prefer-const
-    let { code, message, stack } = error.toJSON();
-    console.error(stack);
-
-    if (code && message) {
-        // Всё есть
-    } else if (!code && !message) {
-        code = 500;
-        message = 'Internal Server Error';
-    } else if (code) {
-        message = `Request failed with status code ${code}`;
-    } else if (message) {
-        code = parseInt(message.replace('Request failed with status code ', ''));
-        if (!code) {
-            code = 500;
-        }
-    }
-
-    return { code, message };
-};
 
 // Простенькое логирование всех запросов к API
 router.use((req, res, next) => {
@@ -77,106 +16,24 @@ router.use((req, res, next) => {
 // Получение и сохранение настроек
 router.route('/settings')
     .all(requireAuthenticationToDB)
-    .get(async (req, res) => {
-        try {
-            const { data } = await axiosDBInstance.get('/conf');
-            return res.json(data);
-        } catch (error) {
-            const { code, message } = getCodeAndMessage(error);
-            return res.status(code).json({ code, message });
-        }
-    })
-    .post(async (req, res) => {
-        try {
-            const { repoName, buildCommand, mainBranch, period } = req.body;
-
-            if (!repoName || !buildCommand || !mainBranch || Number.isNaN(period)) {
-                const code = 400;
-                const message = 'Заполнены не все поля';
-                return res.status(code).json({ code, message });
-            }
-
-            try {
-                await runGitClone(repoName);
-            } catch (error) {
-                const code = 400;
-                const message = 'Не удалось создать локальную копию репозитория';
-                return res.status(code).json({ code, message });
-            }
-            await axiosDBInstance.post('/conf', { ...req.body });
-            const { data } = await axiosDBInstance.get('/conf');
-            return res.json(data);
-        } catch (error) {
-            const { code, message } = getCodeAndMessage(error);
-            return res.status(code).json({ code, message });
-        }
-    });
+    .get(getSettingsRoute)
+    .post(saveSettingsRoute);
 
 // Получение списка сборок и добавление сборки в очередь
 router.route('/builds')
     .all(requireAuthenticationToDB)
-    .get(async (req, res) => {
-        try {
-            const params = { ...req.query };
-
-            const { data } = await axiosDBInstance.get('/build/list', { params });
-            return res.json(data);
-        } catch (error) {
-            const { code, message } = getCodeAndMessage(error);
-            return res.status(code).json({ code, message });
-        }
-    })
-    .post(async (req, res) => {
-        const { commitHash } = req.body;
-        let commit = null;
-
-        try {
-            const { data } = await axiosDBInstance.get('/conf');
-            commit = await getCommitByHash(commitHash, data.data);
-        } catch (error) {
-            const code = 400;
-            const message = `Не удалось получить данные для commitHash: ${commitHash}`;
-            return res.status(code).json({ code, message });
-        }
-
-        try {
-            const { data } = await axiosDBInstance.post('/build/request', commit);
-            return res.json(data);
-        } catch (error) {
-            const { code, message } = getCodeAndMessage(error);
-            return res.status(code).json({ code, message });
-        }
-    });
+    .get(getBuildListRoute)
+    .post(saveNewBuildRoute);
 
 // Получение информации о конкретной сборке
 router.route('/builds/:buildId')
     .all(requireAuthenticationToDB)
-    .get(async (req, res) => {
-        try {
-            const params = { ...req.params };
-
-            const { data } = await axiosDBInstance.get('/build/details', { params });
-            return res.json(data);
-        } catch (error) {
-            const { code, message } = getCodeAndMessage(error);
-            return res.status(code).json({ code, message });
-        }
-    });
+    .get(getBuildDetailsRoute);
 
 // Получение логов билда
 router.route('/builds/:buildId/logs')
     .all(requireAuthenticationToDB)
-    .get(async (req, res) => {
-        try {
-            const params = { ...req.params };
-
-            const { data } = await axiosDBInstance.get('/build/log', { params });
-            return res.json(data);
-        } catch (error) {
-            const { code, message } = getCodeAndMessage(error);
-            return res.status(code).json({ code, message });
-        }
-    });
+    .get(getBuildLogRoute);
 
 // Подключение Swagger. Последний в списке, чтобы не ломать другие маршруты
 router.use('/', swaggerUi.serve, swaggerUi.setup(swaggerConfig));
---
+++ 3-777-NodeJS/db/dbHelper.js
@@ -0,0 +1,179 @@
+const axios = require('axios').default;
+const { Agent } = require('https');
+const { apiToken } = require('../config');
+
+/**
+ * Получение токена для авторизации в базе данных
+ * @param {Object} req
+ * @return {string}
+ */
+const getAuthenticationToken = (req) => {
+    let authorization = req.get('authorization');
+
+    if (!authorization && apiToken) {
+        // Установка специального токена из файла конфигурации
+        authorization = `Bearer ${apiToken}`;
+    }
+
+    return authorization || '';
+};
+
+/**
+ * Проверка токена на наличие схемы и ключа
+ * @param {string} authorization
+ * @return {boolean}
+ */
+const validateAuthenticationToken = (authorization) => {
+    if (!authorization) return false;
+
+    const [scheme, key] = authorization.toString().split(' ');
+
+    return Boolean(scheme === 'Bearer' && key);
+};
+
+/**
+ * Проверка токена для подключения к базе данных
+ * @param {Object} req
+ * @param {Object} res
+ * @param {Function} next
+ * @return {Object}
+ */
+const requireAuthenticationToDB = (req, res, next) => {
+    const authorization = getAuthenticationToken(req);
+
+    if (validateAuthenticationToken(authorization)) {
+        return next();
+    }
+
+    const code = 401;
+    const message = 'Возможны 2 варианта авторизации в БД:'
+        + ' 1. В headers передать authorization: "Bearer {apiToken}".'
+        + ' Для этого можно использовать кнопку Authorize в Swagger'
+        + ' 2. В .env файле указать apiToken, как в файле примере(.env.example).'
+        + ' Не забыть потом перезапустить сервер';
+
+    return res.status(code).json({ code, message });
+};
+
+/**
+ * Создание подключения к базе данных
+ * @param {Object} req
+ * @return {Object}
+ */
+const createDBInstance = (req) => axios.create({
+    baseURL: 'https://hw.shri.yandex/api',
+    timeout: 5000,
+    headers: {
+        Authorization: getAuthenticationToken(req),
+    },
+    httpsAgent: new Agent({
+        rejectUnauthorized: false,
+    }),
+});
+
+/**
+ * Получение настроек
+ * @param {Object} req
+ * @return {Object}
+ */
+const getSettingsFromDB = async (req) => {
+    const db = createDBInstance(req);
+    const { data } = await db.get('/conf');
+
+    return data;
+};
+
+/**
+ * Сохранение настроек
+ * @param {Object} req
+ * @param {Object} settings
+ * @return {Object}
+ */
+const saveSettingsToDB = async (req, settings) => {
+    const db = createDBInstance(req);
+    return db.post('/conf', settings);
+};
+
+/**
+ * Получение списка сборок
+ * @param {Object} req
+ * @param {Object} params
+ * @return {Object}
+ */
+const getBuildListFromDB = async (req, params) => {
+    const db = createDBInstance(req);
+    const { data } = await db.get('/build/list', { params });
+
+    return data;
+};
+
+/**
+ * Сохранение новой сборки
+ * @param {Object} req
+ * @param {Object} commit
+ * @return {Object}
+ */
+const saveNewBuildToDB = async (req, commit) => {
+    const db = createDBInstance(req);
+    const { data } = await db.post('/build/request', commit);
+
+    return data;
+};
+
+/**
+ * Получение информации о конкретной сборке
+ * @param {Object} req
+ * @param {Object} params
+ * @return {Object}
+ */
+const getBuildDetailsFromDB = async (req, params) => {
+    const db = createDBInstance(req);
+    const { data } = await db.get('/build/details', { params });
+
+    return data;
+};
+
+/**
+ * Получение логов билда
+ * @param {Object} req
+ * @param {Object} params
+ * @return {Object}
+ */
+const getBuildLogFromDB = async (req, params) => {
+    const db = createDBInstance(req);
+    const { data } = await db.get('/build/log', { params });
+
+    return data;
+};
+
+const getCodeAndMessage = (error) => {
+    let { code, message } = error;
+
+    if (code && message) {
+        // Всё есть
+    } else if (!code && !message) {
+        code = 500;
+        message = 'Internal Server Error';
+    } else if (code) {
+        message = `Request failed with status code ${code}`;
+    } else if (message) {
+        code = parseInt(message.replace('Request failed with status code ', ''));
+        if (!code) {
+            code = 500;
+        }
+    }
+
+    return { code, message };
+};
+
+module.exports = {
+    validateAuthenticationToken,
+    requireAuthenticationToDB,
+    getSettingsFromDB,
+    saveSettingsToDB,
+    getBuildListFromDB,
+    saveNewBuildToDB,
+    getBuildDetailsFromDB,
+    getBuildLogFromDB,
+    getCodeAndMessage,
+};
